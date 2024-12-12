f12fa3ff94a06d8b2b16d13a94636e974c389238
fumiya <156275318+fumiya95@users.noreply.github.com>
2024-12-12 23:09:16 +0900

    Update database.py

--- fast_api_todos/database.py
+++ fast_api_todos/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = "mysql+pymysql://root:root@localhost:3306/fast_api_todo_db"
+
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine
