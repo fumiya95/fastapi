f1225451a860687fdd491eca8b7cdc7adf273cb0
fumiya <156275318+fumiya95@users.noreply.github.com>
2024-12-18 12:20:06 +0900

    Add files via upload

--- fast_api_todos/routers/todos.py
+++ fast_api_todos/routers/todos.py
@@ -4,6 +4,7 @@ from database import SessionLocal, get_db
 from typing import Annotated, Optional
 from sqlalchemy.orm import Session
 from models import Todos
+from .auth import get_current_user
 
 router = APIRouter( prefix='/todo',
 
@@ -11,6 +12,7 @@ router = APIRouter( prefix='/todo',
 
 # main.pyから機能を移植
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=1,max_length=255)
@@ -30,9 +32,12 @@ async def read_todo(db: db_dependency,todo_id:int=Path(gt=0)):
     raise HTTPException(status_code=404, detail="todo not found")
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
+async def create_todo(user:user_dependency,db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+       raise HTTPException(status_code=401, detail='Authentication Failed')
     # 新しい TODO を作成する
-    todo_model = Todos(**todo_request.dict())
+    todo_model = Todos(**todo_request.dict(), user_id=user.get('id'))
+    
     db.add(todo_model)
     db.commit()
 
